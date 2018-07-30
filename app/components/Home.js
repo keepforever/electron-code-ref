e144eefa383121a142917c9ce748afd5d7e2cb45
Brian Cilenti <briancilenti@gmail.com>
2018-07-29 17:55:59 -0700

    l

--- app/components/Home.js
+++ app/components/Home.js
@@ -26,7 +26,7 @@ export default class Home extends Component<Props> {
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
-          <Link to="/counter">Lick my balls</Link>
+          <Link to="/counter">Counter</Link>
           {Languages.vals.map((lang, index) => {
             return (
               <div className={styles.languages} key={index}>
